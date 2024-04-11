91d27a8a45e2c400d04a2c555a5641083fd405fd
nichokap <kapros@ebox.gr>
2024-04-11 12:22:55 +0300

    Update main.py

--- Examples/Built-in LED/main.py
+++ Examples/Built-in LED/main.py
@@ -6,7 +6,7 @@ from neopixel import NeoPixel
 strip = NeoPixel(Pin(25), 1)
 
 #Set the color of the first and only LED
-strip[0] = (0,150,0) # color codes in RGB
+strip[0] = (0,150,0) # color codes in GRB (Green=0, Red=150, Blue=0)
 
 #command to write the color to the LED
 strip.write()
