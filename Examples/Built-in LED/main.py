5a99cf4d562449711c4faaccfef454b9e0644a5e
nichokap <kapros@ebox.gr>
2024-04-11 12:18:37 +0300

    Update main.py

--- Examples/Built-in LED/main.py
+++ Examples/Built-in LED/main.py
@@ -2,7 +2,11 @@ from machine import Pin
 from neopixel import NeoPixel
 
 
-#Configure the built-in LED
+#Configure the built-in WS2812 LED pins and set the number of LEDs to 1
 strip = NeoPixel(Pin(25), 1)
+
+#Set the color of the first and only LED
 strip[0] = (0,150,0) # color codes in RGB
+
+#command to write the color to the LED
 strip.write()
